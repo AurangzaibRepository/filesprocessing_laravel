d98bbda6ad0c4ba5aa9fc1b3c9febdf7ff882a27
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-07 15:41:16 +0500

    ADefines rules

--- app/Http/Requests/FileUploadRequest.php
+++ app/Http/Requests/FileUploadRequest.php
@@ -3,12 +3,17 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\File;
 
 class FileUploadRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
+            'input-file' => [
+                'required',
+                File::types(['csv']),
+            ],
         ];
     }
 }
