4b5eee7de11b4ae0ecc179c5e538b5afbedf8230
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-07 23:29:18 +0500

    Specifies olumns

--- database/migrations/2023_10_07_181853_create_file_records_table.php
+++ database/migrations/2023_10_07_181853_create_file_records_table.php
@@ -10,6 +10,15 @@ return new class extends Migration
     {
         Schema::create('file_records', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('unique_key');
+            $table->string('product_title', 50);
+            $table->text('product_description');
+            $table->string('style_no', 20);
+            $table->string('sanmar_mainframe_color', 30);
+            $table->string('size', 10);
+            $table->string('color_name', 30);
+            $table->double('piece_price', 6, 2);
+            $table->foreignId('file_id');
             $table->timestamps();
         });
     }
