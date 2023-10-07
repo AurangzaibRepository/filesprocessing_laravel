6c0e40b220e82654a94bc821f592d49681f311b3
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-07 20:48:51 +0500

    Adds name column

--- database/migrations/2023_10_07_154425_create_files_table.php
+++ database/migrations/2023_10_07_154425_create_files_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 200);
             $table->timestamps();
         });
     }
