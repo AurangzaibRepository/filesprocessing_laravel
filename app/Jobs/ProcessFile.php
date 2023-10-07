c5e41950902e690d54f67a6316db24de5c42b856
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-07 23:58:50 +0500

    Passes parameters to job

--- app/Jobs/ProcessFile.php
+++ app/Jobs/ProcessFile.php
@@ -2,8 +2,8 @@
 
 namespace App\Jobs;
 
+use App\Models\File;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
@@ -12,13 +12,11 @@ use Illuminate\Queue\SerializesModels;
 class ProcessFile implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    private $fileId;
-    private $fileName;
 
-    public function __construct($fileId, $fileName)
-    {
-        $this->fileId = $fileId;
-        $this->fileName = $fileName;
+    public function __construct(
+        private File $file,
+        private string $fileName,
+    ) {
     }
 
     public function handle(): void
