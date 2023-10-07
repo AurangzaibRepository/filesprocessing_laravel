ee85aca46219580c4d8b1d38a4884bcc6b9d4589
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-07 21:36:01 +0500

    Impelemtns function to sve file

--- app/Models/File.php
+++ app/Models/File.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class File extends Model
 {
@@ -10,4 +12,25 @@ class File extends Model
         'name',
         'status',
     ];
+
+    public function saveRecord(Request $request): void
+    {
+        $fileObject = $request->file('inputfile');
+
+        $file = $this->create([
+            'name' => $fileObject->getClientOriginalName(),
+        ]);
+
+        $this->saveFile($fileObject, $file->id);
+    }
+
+    private function saveFile($fileObject, int $id): void
+    {
+        $fileBaseName = Str::of($fileObject->getClientOriginalName())->explode('.')[0];
+
+        $fileObject->move(
+            public_path('files'),
+            "{$fileBaseName}_{$id}.{$fileObject->getClientOriginalExtension()}"
+        );
+    }
 }
