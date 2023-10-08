6d8a463751a8d04e36afecb36613a5c9913fa841
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-08 15:17:23 +0500

    Incldue shleper in conffig app

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+        'FormatHelper' => App\Helpers\FormatHelper::class,
     ])->toArray(),
 
 ];
