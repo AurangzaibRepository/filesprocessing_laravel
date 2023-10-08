22dc2b8e3c4274a682e051a43e04b20169129e5a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-08 16:12:11 +0500

    Calls model function to save records

--- app/Jobs/ProcessFile.php
+++ app/Jobs/ProcessFile.php
@@ -16,6 +16,8 @@ class ProcessFile implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $timeout = 20000;
+
     public function __construct(
         private File $file,
         private string $fileName,
@@ -27,41 +29,13 @@ class ProcessFile implements ShouldQueue
         $this->file->updateStatus($this->file->id, 'processing');
         $data = FileHelper::parseFile($this->fileName);
 
-        $this->addRecords($data);
+        FileRecord::addRecords($data);
 
         $this->file->updateStatus($this->file->id, 'completed');
     }
 
-    private function addRecords(array $data): void
+    public function failed(Throwable $exception): void
     {
-        foreach ($data as $row) {
-            $productTitle = FormatHelper::cleanString($row['PRODUCT_TITLE'], 'UTF-8');
-            $productDescription = FormatHelper::cleanString($row['PRODUCT_DESCRIPTION'], 'UTF-8');
-
-            $attributeList = [
-                'unique_key' => $row['UNIQUE_KEY'],
-                'product_title' => $productTitle,
-                'product_description' => $productDescription,
-                'style_no' => $row['STYLE#'],
-                'sanmar_mainframe_color' => $row['SANMAR_MAINFRAME_COLOR'],
-                'size' => $row['SIZE'],
-                'color_name' => $row['COLOR_NAME'],
-                'piece_price' => $row['PIECE_PRICE'],
-            ];
-
-            FileRecord::upsert([
-                $attributeList,
-                $attributeList,
-            ],
-            [
-                'product_title',
-                'product_description',
-                'style_no',
-                'sanmar_mainframe_color',
-                'size',
-                'color_name',
-                'piece_price',
-            ]);
-        }
+        $this->file->updateStatus($this->file->id, 'failed');
     }
 }
