3eca752255f3aaf54c1095ad6e583db785ffc769
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-08 19:22:09 +0500

    Creates spearat function to add single record in Db

--- app/Models/FileRecord.php
+++ app/Models/FileRecord.php
@@ -25,7 +25,7 @@ class FileRecord extends Model
         return $this->belongsTo(File::class);
     }
 
-    public static function addRecords(array $data): void
+    /*public static function addRecords(array $data): void
     {
         foreach ($data as $row) {
             $productTitle = FormatHelper::cleanString($row['PRODUCT_TITLE'], 'UTF-8');
@@ -56,5 +56,36 @@ class FileRecord extends Model
                 'piece_price',
             ]);
         }
+    }*/
+
+    public static function addRecords(array $row): void
+    { 
+        $productTitle = FormatHelper::cleanString($row['PRODUCT_TITLE'], 'UTF-8');
+        $productDescription = FormatHelper::cleanString($row['PRODUCT_DESCRIPTION'], 'UTF-8');
+
+        $attributeList = [
+            'unique_key' => $row['UNIQUE_KEY'],
+            'product_title' => $productTitle,
+            'product_description' => $productDescription,
+            'style_no' => $row['STYLE#'],
+            'sanmar_mainframe_color' => $row['SANMAR_MAINFRAME_COLOR'],
+            'size' => $row['SIZE'],
+            'color_name' => $row['COLOR_NAME'],
+            'piece_price' => $row['PIECE_PRICE'],
+        ];
+
+        FileRecord::upsert([
+            $attributeList,
+            $attributeList,
+        ],
+        [
+            'product_title',
+            'product_description',
+            'style_no',
+            'sanmar_mainframe_color',
+            'size',
+            'color_name',
+            'piece_price',
+        ]);   
     }
 }
