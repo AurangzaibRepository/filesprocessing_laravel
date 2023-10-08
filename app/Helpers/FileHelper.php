811ead1be89b6e28f7b9870bc690b87ed1a4eb60
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-08 19:21:51 +0500

    Creates separar function to add single row in memory

--- app/Helpers/FileHelper.php
+++ app/Helpers/FileHelper.php
@@ -2,9 +2,11 @@
 
 namespace App\Helpers;
 
+use App\Models\FileRecord;
+
 class FileHelper
 {
-    public static function parseFile(string $fileName): array
+   /* public static function parseFile(string $fileName): array
     {
         $data = [];
         $filePath = public_path('\files\\').$fileName;
@@ -19,5 +21,21 @@ class FileHelper
         fclose($fileReader);
 
         return $data;
+    }*/
+
+    public static function parseFile(string $fileName): void
+    {
+        $data = null;
+        $filePath = public_path('\files\\').$fileName;
+        $fileReader = fopen($filePath, 'r');
+
+        $header = fgetcsv($fileReader);
+
+        while ($row = fgetcsv($fileReader)) {
+            $data = array_combine($header, $row);
+            FileRecord::addRecords($data);
+        }
+
+        fclose($fileReader);
     }
 }
