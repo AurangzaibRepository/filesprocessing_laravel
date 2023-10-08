33646e7d5427e8780f1b610f67f6d259dae6f4d2
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-08 16:11:52 +0500

    Impleemmnts fnctin to add records

--- app/Models/FileRecord.php
+++ app/Models/FileRecord.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use FormatHelper;
 
 class FileRecord extends Model
 {
@@ -22,5 +23,38 @@ class FileRecord extends Model
     public function file(): Relation
     {
         return $this->belongsTo(File::class);
+    }
+
+    public static function addRecords(array $data): void
+    {
+        foreach ($data as $row) {
+            $productTitle = FormatHelper::cleanString($row['PRODUCT_TITLE'], 'UTF-8');
+            $productDescription = FormatHelper::cleanString($row['PRODUCT_DESCRIPTION'], 'UTF-8');
+
+            $attributeList = [
+                'unique_key' => $row['UNIQUE_KEY'],
+                'product_title' => $productTitle,
+                'product_description' => $productDescription,
+                'style_no' => $row['STYLE#'],
+                'sanmar_mainframe_color' => $row['SANMAR_MAINFRAME_COLOR'],
+                'size' => $row['SIZE'],
+                'color_name' => $row['COLOR_NAME'],
+                'piece_price' => $row['PIECE_PRICE'],
+            ];
+
+            FileRecord::upsert([
+                $attributeList,
+                $attributeList,
+            ],
+            [
+                'product_title',
+                'product_description',
+                'style_no',
+                'sanmar_mainframe_color',
+                'size',
+                'color_name',
+                'piece_price',
+            ]);
+        }
     }
 }
