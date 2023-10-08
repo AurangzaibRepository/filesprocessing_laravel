81177954c86d9c9780c3edb229a507b343d96567
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-08 11:54:34 +0500

    Speifies chaarcterset

--- database/migrations/2023_10_07_181853_create_file_records_table.php
+++ database/migrations/2023_10_07_181853_create_file_records_table.php
@@ -9,9 +9,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('file_records', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
             $table->id();
             $table->bigInteger('unique_key');
-            $table->string('product_title', 50);
+            $table->string('product_title', 300);
             $table->text('product_description');
             $table->string('style_no', 20);
             $table->string('sanmar_mainframe_color', 30);
