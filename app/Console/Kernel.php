bf1b16b1a2781ab3cf74600520017921446a51a0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-08 19:22:21 +0500

    Comments chedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new ProcessFile)->everyFiveMinutes();
+        //$schedule->job(new ProcessFile)->everyFiveMinutes();
     }
 
     protected function commands(): void
