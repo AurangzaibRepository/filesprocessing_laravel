a042e74d86fc75574e18a7a7d8bf8dd32deb0fde
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-08 15:32:05 +0500

    Schedule job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,20 +4,16 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\ProcessFile;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
+
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new ProcessFile)->everyFiveMinutes();
     }
 
-    /**
-     * Register the commands for the application.
-     */
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
