3925b8a106c36f756b389b32b86d2dc6f359517d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-10-09 11:19:49 +0500

    Creates even tfor status push

--- app/Events/FileStatusUpdate.php
+++ app/Events/FileStatusUpdate.php
@@ -4,13 +4,11 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class FileStatusUpdate
+class FileStatusUpdate implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -18,10 +16,13 @@ class FileStatusUpdate
     {
     }
 
-    public function broadcastOn(): array
+    public function broadcastAs()
     {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
+        return 'filestatusevent';
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('filestatus');
     }
 }
